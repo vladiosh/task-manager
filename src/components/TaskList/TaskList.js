6ffc390f7e4617558a0b1918f146dd0471844119
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2023-01-03 20:00:21 +0200

    підписка на store

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,10 +1,31 @@
+import { useSelector } from 'react-redux';
 import { Task } from 'components/Task/Task';
+import { statusFilters } from '../../redux/constants';
+import { getTasks, getStatusFilter } from '../../redux/selectors';
 import css from './TaskList.module.css';
 
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
+
 export const TaskList = () => {
+  // Получаем массив задач из состояния Redux
+  const tasks = useSelector(getTasks);
+  // Получаем значение фильтра из состояния Redux
+  const statusFilter = useSelector(getStatusFilter);
+  // Вычисляем массив задач которые необходимо отображать в интерфейсе
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+
   return (
     <ul className={css.list}>
-      {[].map(task => (
+      {visibleTasks.map(task => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
