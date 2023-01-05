46b12c212270a60d1de0080e1d32b14b71edb4dd
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2023-01-05 09:47:03 +0200

    in progress

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,16 +1,29 @@
+// Импортируем хук
+import { useDispatch } from 'react-redux';
+// Импортируем генератор экшена
+import { deleteTask } from '../../redux/actions';
+import { toggleCompleted } from '../../redux/actions';
 import { MdClose } from 'react-icons/md';
 import css from './Task.module.css';
 
 export const Task = ({ task }) => {
+  const dispatch = useDispatch();
+  // Вызываем генератор экшена и передаём идентификатор задачи
+  // Отправляем результат - экшен удаления задачи
+  const handleDelete = () => dispatch(deleteTask(task.id));
+  // Вызываем генератор экшена и передаём идентификатор задачи
+  // Отправляем результат - экшен переключения статуса задачи
+  const handleToggle = () => dispatch(toggleCompleted(task.id));
   return (
     <div className={css.wrapper}>
       <input
         type="checkbox"
         className={css.checkbox}
+        onChange={handleToggle}
         checked={task.completed}
       />
       <p className={css.text}>{task.text}</p>
-      <button className={css.btn}>
+      <button className={css.btn} onClick={handleDelete}>
         <MdClose size={24} />
       </button>
     </div>
