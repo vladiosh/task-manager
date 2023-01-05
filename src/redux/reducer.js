52d994ff21c1abb34682a064d1e3661afcd05b86
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2023-01-05 17:26:44 +0200

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -52,13 +52,7 @@ import { statusFilters } from './constants';
 //   }
 // };
 
-const tasksInitialState = [
-  { id: 0, text: 'Learn HTML and CSS', completed: true },
-  { id: 1, text: 'Get good at JavaScript', completed: true },
-  { id: 2, text: 'Master React', completed: false },
-  { id: 3, text: 'Discover Redux', completed: false },
-  { id: 4, text: 'Build amazing apps', completed: false },
-];
+const tasksInitialState = [];
 
 // Отвечает только за обновление свойства tasks
 // Теперь значением параметра state будет массив задач
